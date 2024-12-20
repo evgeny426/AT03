a944f295b533bb26c5d1345de1e7ad1bce7a7bfc
evgeny426 <bardygin1@yandex.ru>
2024-12-20 22:44:56 +0400

    fixed test_hw.py

--- test_hw.py
+++ test_hw.py
@@ -3,7 +3,7 @@ from main_hw import get_cat
 
 
 def test_get_cat_success(mocker):
-    mock_get = mocker.patch('main.requests.get')
+    mock_get = mocker.patch('main_hw.requests.get')
     mock_get.return_value.status_code = 200
     mock_get.return_value.json.return_value = {
         "id":"MTcwMTAxNg",
